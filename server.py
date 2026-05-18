31dcc727504bacab46e02410fa19b0ea366447ea
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-05-18 14:27:21 +0300

    added weather updater to the main server file

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 import http.server
-import os
+import weather_updater
 
 PORT = 9999
 DIRECTORY = "resources"
 
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=DIRECTORY,**kwargs)
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
 
 
-with http.server.HTTPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at http://localhost:{PORT}")
-    httpd.serve_forever()
+if __name__ == "__main__":
+    # Start weather cache updater
+    weather_updater.update_weather()
+
+    with http.server.HTTPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        httpd.serve_forever()
