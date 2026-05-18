76eaebca656b494009c502c136087499e3e5a73b
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-05-18 12:33:41 +0300

    declared variables for weather api call

--- script.js
+++ script.js
@@ -18,5 +18,10 @@ setTimeout(() => {
     });
 }, 800);
 
+// === WEATHER ===
+const nairobiCoordinates = [-1.2302285, 36.866615]
+const openMeteoApi = "https://api.open-meteo.com/v1/"
+
+const link = `${openMeteoApi}forecast?latitude=${nairobiCoordinates[0]}&longitude=${nairobiCoordinates[1]}&current=temperature_2m,weathercode&timezone=Africa/Nairobi`;
 
 
