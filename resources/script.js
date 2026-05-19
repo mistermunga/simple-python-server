eb18fa1e0dac73a62808860faba4966f336361ca
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-05-19 09:44:14 +0300

    call the new endpoint for weather

--- resources/script.js
+++ resources/script.js
@@ -19,13 +19,8 @@ setTimeout(() => {
 }, 800);
 
 // === WEATHER ===
-const nairobiCoordinates = [-1.2302285, 36.866615]
-const openMeteoApi = "https://api.open-meteo.com/v1/"
-
-const link = `${openMeteoApi}forecast?latitude=${nairobiCoordinates[0]}&longitude=${nairobiCoordinates[1]}&current=temperature_2m,weathercode&timezone=Africa/Nairobi`;
-
 async function fetchWeather() {
-    const response = await fetch(link);
+    const response = await fetch("/weather");
     return await response.json();
 }
 
@@ -33,10 +28,11 @@ async function updateWeather() {
     try {
         const data = await fetchWeather();
         document.getElementById("weather").textContent =
-            `${data.current.temperature_2m}°C`;
+            `${data.temperature}°C`;
     } catch (error) {
         document.getElementById("weather").textContent = "–";
     }
 }
 
 updateWeather();
+setInterval(updateWeather, 60000); // refresh display every minute
